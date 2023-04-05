f5723e031a51d6f6f622dfb2e4612eed053fb219
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-04-05 22:58:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-21858532 - 01f8fabf05f69063186fd3644
-
 function fetchCountries(country) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${country}?fields=name,capital,population,flags,languages`
+    `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
