820386b802b12155610fb86f3444f12f80312e1e
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-04-05 23:00:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function fetchCountries(country) {
   return fetch(
-    `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644`
+    `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=yellow+flowers&image_type=photo`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
