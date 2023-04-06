53bb5c26af621b2a4389a5eaaddc4a90ac84213b
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-04-06 16:58:21 +0200

    u

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const imagesForm = document.querySelector('#search-form');
 const listImages = document.querySelector('.gallery');
 
 function fetchImages(name) {
-  return axios.get(
-    `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=yellow+flowers&image_type=photo`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios
+    .get(
+      `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=yellow+flowers&image_type=photo`
+    )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
 }
 
 // countryBox.addEventListener('input', debounce(searchCountris, DEBOUNCE_DELAY));
@@ -40,23 +42,26 @@ function renderImages(image) {
   console.log(image);
 }
 
-const card {
-<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>;
-}
-
+const card = image
+  .map(image => {
+    <div class="photo-card">
+      <img src="" alt="" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+        </p>
+      </div>
+    </div>;
+  })
+  .join('');
+listImages.innerHTML = markup;
+return;
