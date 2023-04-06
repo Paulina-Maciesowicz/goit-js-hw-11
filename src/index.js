c1e08894883fde2276a525cdcfb5a1fe69874fd1
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-04-06 16:42:17 +0200

    u

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
+
 const imagesForm = document.querySelector('#search-form');
 const listImages = document.querySelector('.gallery');
 
 function fetchImages(name) {
-  return fetch(
+  return axios.get(
     `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=yellow+flowers&image_type=photo`
   ).then(response => {
     if (!response.ok) {
