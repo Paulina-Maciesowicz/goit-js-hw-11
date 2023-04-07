8ef7f48b2a60fad62b30b67e58ed10caabc4a7d9
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-04-07 19:18:55 +0200

    u

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import Notiflix from 'notiflix';
 const imagesForm = document.querySelector('#search-form');
 const listImages = document.querySelector('.gallery');
 // const moreImages = document.querySelector('#load-more');
+let page = 1
 
 function fetchImages(name) {
   return axios
     .get(
-      `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=yellow+flowers&image_type=photo`
+      `https://pixabay.com/api/?key=21858532-01f8fabf05f69063186fd3644&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=3`
     )
     .then(response => {
       return response.data;
@@ -27,7 +28,7 @@ function searchImages(event) {
 
   listImages.innerHTML = '';
   {
-    fetchImages(abc)
+    fetchImages(searchQuery.value)
       .then(images => renderImages(images))
       .catch(error => {
         Notiflix.Notify.failure('Oops, there is no image with that name');
